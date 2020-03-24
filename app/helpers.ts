c27d06efc2d3571945b1bc6c84d4da9f94030c9d
sergiooso <60956088+sergiooso@users.noreply.github.com>
2020-03-23 21:13:47 -0600

    First commit

--- app/helpers.ts
+++ app/helpers.ts
@@ -1,15 +1,5 @@
 import mongoose = require("mongoose");
-import { resolve } from "dns";
+import {resolve} from "dns";
 
-const uri: string = "mongodb+srv://sjavila012:(sergiocolita1)@cluster0-9uoro.mongodb.net/ventas?retryWrites=true&w=majority";
-
-export const connectMongoDB  = new Promise<void>(resolve => {
-    mongoose.connect(uri,{ useNewUrlParser:true, useUnifiedTopology: true }, (err: any) => {
-        if(err){
-            console.log(err.message);
-        }else{
-            console.log("Conexion exitosa");
-        }
-        resolve();
-    });
-});
+const uri : string = "mongodb+srv://sjavila012:(sergiocolita1)@cluster0-9uoro.mongodb.net/ventas?retryWrites=true&w=majority";
+    
